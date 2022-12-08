a2b82cb91c94a359f97f146602c0b95bc4109f88
Bruno Luz <bruno.al.luz@gmail.com>
2022-12-07 23:20:39 -0300

    using secrets

--- appservice/main.tf
+++ appservice/main.tf
@@ -2,19 +2,20 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-resource "aws_s3_bucket" "state_bucket" {
-  bucket = "${var.prefix}-bucket-bruno-xpto"
-
-  # Tells AWS to encrypt the S3 bucket at rest by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
- tags = {
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mybucket"
+  
+  tags = {
     Terraform = "true"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "mybucket_encryption" {
+  bucket = aws_s3_bucket.mybucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
