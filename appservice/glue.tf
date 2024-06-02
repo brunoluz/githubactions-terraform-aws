9142b1100beca59d72b4e1dabfc3eab6a64eda3d
Bruno Luz <bruno.al.luz@gmail.com>
2024-06-02 13:05:59 -0300

    glue table

--- appservice/glue.tf
+++ appservice/glue.tf
@@ -46,7 +46,7 @@ resource "aws_glue_catalog_table" "glue_table" {
 
 resource "aws_lakeformation_permissions" "perm1" {
   principal                     = aws_iam_role.test_glue_role.arn
-  permissions                   = ["SELECT"]
+  permissions                   = ["SELECT", "ALTER", "INSERT"]
   table {
     database_name = aws_glue_catalog_database.glue_db.name
     #name = aws_glue_catalog_table.glue_table.name
