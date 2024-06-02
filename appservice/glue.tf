163a4b44063293dae220ba56749d2c95264da26f
Bruno Luz <bruno.al.luz@gmail.com>
2024-06-02 11:08:45 -0300

    glue table

--- appservice/glue.tf
+++ appservice/glue.tf
@@ -1,3 +1,8 @@
 resource "aws_glue_catalog_database" "glue_db" {
   name = "glue_db"
 }
+
+resource "aws_glue_catalog_table" "glue_table" {
+  name          = "glue_table"
+  database_name = glue_db.name
+}
