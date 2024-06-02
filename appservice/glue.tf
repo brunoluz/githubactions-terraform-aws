80e9b96e7f6d4da42ee14ce631f0beba6a63a842
Bruno Luz <bruno.al.luz@gmail.com>
2024-06-02 11:26:50 -0300

    glue table

--- appservice/glue.tf
+++ appservice/glue.tf
@@ -1,7 +1,19 @@
 resource "aws_iam_role" "test_glue_role" {
   name = "test_glue_role"
 
-  assume_role_policy = ""
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
   inline_policy {
     name = "glue_policy"
