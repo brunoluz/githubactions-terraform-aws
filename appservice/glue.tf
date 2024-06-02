0fd85218f9f5011909f1ec734926fa629f97e86f
Bruno Luz <bruno.al.luz@gmail.com>
2024-06-02 11:24:41 -0300

    glue table

--- appservice/glue.tf
+++ appservice/glue.tf
@@ -1,3 +1,21 @@
+resource "aws_iam_role" "test_glue_role" {
+  name = "test_glue_role"
+
+  assume_role_policy = ""
+
+  inline_policy {
+    name = "glue_policy"
+    policy = jsonencode({
+      Version = "2012-10-17"    
+      Statement = [{
+        Action = ["glue:*"]
+        Effect   = "Allow"
+        Resource = "*"
+      }]
+    })
+  }
+}
+
 resource "aws_glue_catalog_database" "glue_db" {
   name = "glue_db"
 }
