81bfe63aeb5d031c23d07c7ddca162bc49dff820
Bruno Luz <bruno.al.luz@gmail.com>
2024-06-02 09:42:33 -0300

    testes

--- appservice/main.tf
+++ appservice/main.tf
@@ -1,27 +1,3 @@
 provider "aws" {
   region = "sa-east-1"
 }
-
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "${var.prefix}-githubactions-terraform-aws-teste"
-  
-  tags = {
-    Terraform = "true"
-  }
-}
-
-resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.mybucket.bucket
-  key    = "new_object_key"
-  source = "file.txt"
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "mybucket_encryption" {
-  bucket = aws_s3_bucket.mybucket.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
