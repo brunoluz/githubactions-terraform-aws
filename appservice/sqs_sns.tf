9b75ee48c861dbe0e640a5cc4fcc145bc87c19b9
Bruno Luz <bruno.al.luz@gmail.com>
2024-06-02 20:07:10 -0300

    sns fifo

--- appservice/sqs_sns.tf
+++ appservice/sqs_sns.tf
@@ -3,3 +3,9 @@ resource "aws_sqs_queue" "terraform_fifo_queue" {
   fifo_queue                  = true
   content_based_deduplication = true
 }
+
+resource "aws_sns_topic" "terraform_fifo_topic" {
+  name                        = "terraform_fifo_topic.fifo"
+  fifo_topic                  = true
+  content_based_deduplication = true
+}
